baab226cfa586e4f8bbdaf3dd8ad8a6d91677ea0
juanpi5 <arcilajusn@gmail.com>
2024-07-17 11:25:14 -0500

    Update intentos.jl

--- intentos.jl
+++ intentos.jl
@@ -1,23 +1,19 @@
 using Plots
 using LinearAlgebra
-#constants 
-G = 6.6743e-11 
-m1 = 5.972e24
-m2 = 7.347e22
-m3 = 1.988e30
 
-function distance(pi, pj)
-    r_ij = sqrt((pj[1]-pi[1])^2 + (pj[2]-pi[2])^2)
-    return r_ij
-end
+#constants 
+m1 = 10.0
+m2 = 10.0
+m3 = 1e-17
 
 function forces(pi, pj, pk, mi, mj, mk)
+
     #r, distance between two planets 
     #p, position of each planet 
     #m, mass of each planet
     r_ij = norm(pj-pi)
     r_ik = norm(pk-pi)
-    F = G*((mi*mj)*(pj-pi)/r_ij^3 + (mi*mk)*(pk-pi)/r_ik^3)
+    F = ((mi*mj)*(pj-pi)/r_ij^3 + (mi*mk)*(pk-pi)/r_ik^3)
 
 end
 
@@ -34,7 +30,6 @@ function derivatives(t, u)
     acc1, acc2, acc3 = F1/m1, F2/m2, F3/m3
 
     #derivatives with respect to u
-
     du = Vector([vel1, vel2, vel3, acc1, acc2, acc3])
 
     return du
@@ -61,46 +56,45 @@ function updated_positions()
 
     #parameters
 
-    p1 = [1.496e11, 0]
-    p2 = [0, 0]
-    p3 = [1.5e11, 3.84e8] 
+    p1 = [3.0, 0.0]
+    p2 = [-3.0, 0.0]
+    p3 = [0.0, 0.0] 
 
-    v1 = [3e4, 3e4]
-    v2 = [-1.1e3, -1.1e3]
-    v3 = [1.4e5, 2.6e3]
+    v1 = [0.0, 1.0]
+    v2 = [0.0, -1.0]
+    v3 = [0.0, 0.0]
 
     u0 = Vector([p1, p2, p3, v1, v2, v3])
     t0 = 0
     dt = 0.1
-    t_max = 50
+    t_max = 100
+    steps = convert(Int64, t_max/dt)
 
-    poss_1 = Vector{Array{Float64}}(undef, convert(Int64, t_max/dt))
-    poss_2 = Vector{Array{Float64}}(undef, convert(Int64, t_max/dt))
-    poss_3 = Vector{Array{Float64}}(undef, convert(Int64, t_max/dt))
+    poss_1 = Vector{Array{Float64}}(undef, steps)
+    poss_2 = Vector{Array{Float64}}(undef, steps)
+    poss_3 = Vector{Array{Float64}}(undef, steps)
 
-    for dt in 1:t_max
+    for i in 1:steps
 
-        poss_1[dt] = copy(u0[1])
-        poss_2[dt] = copy(u0[2])
-        poss_3[dt] = copy(u0[3])
+        poss_1[i] = copy(u0[1])
+        poss_2[i] = copy(u0[2])
+        poss_3[i] = copy(u0[3])
+
         Runge_kutta(u0, t0, dt)
 
     end
 
 # Running the simulation
-    tp = 0
-    
-    @gif for dt in 1:t_max
 
-        tp += 1 
-
-            plot(poss_1[1:tp,1], poss_1[1:tp,2], label="m1", xlims=(-5,5), ylims=(-5,5),  linecolor=:red)
-            plot!(poss_2[1:tp,1], poss_2[1:tp,2], label="m2", xlims=(-5,5), ylims=(-5,5), linecolor=:blue)
-            plot!(poss_3[1:tp,1], poss_3[1:tp,2], label="m3", xlims=(-5,5), ylims=(-5,5), linecolor=:green)
-            scatter!([poss_1[tp,1]], [poss_1[tp,2]], label="", xlims=(-5,5), ylims=(-5,5), markercolor=:red)
-            scatter!([poss_2[tp,1]], [poss_2[tp,2]], label="", xlims=(-5,5), ylims=(-5,5), markercolor=:blue)
-            scatter!([poss_3[tp,1]], [poss_3[tp,2]], label="", xlims=(-5,5), ylims=(-5,5), markercolor=:green)
-
+    @gif for i in 1:steps
+        plot(map(p -> p[1], poss_1[1:i]), map(p -> p[2], poss_1[1:i]), label="m1", xlims=(-20, 20), ylims=(-20, 20), linecolor=:red)
+        plot!(map(p -> p[1], poss_2[1:i]), map(p -> p[2], poss_2[1:i]), label="m2", linecolor=:blue)
+        plot!(map(p -> p[1], poss_3[1:i]), map(p -> p[2], poss_3[1:i]), label="m3", linecolor=:green)
+        scatter!([poss_1[i][1]], [poss_1[i][2]], label="", markercolor=:red)
+        scatter!([poss_2[i][1]], [poss_2[i][2]], label="", markercolor=:blue)
+        scatter!([poss_3[i][1]], [poss_3[i][2]], label="", markercolor=:green)
     end
 
 end
+
+anim = updated_positions()
